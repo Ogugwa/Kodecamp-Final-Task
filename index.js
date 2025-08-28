9117ea81e3cfba4473f2e9b767d7870bbb1f4941
Deborah Okolo <okoloogugwa@gmail.com>
2025-08-28 23:18:19 +0100

    The landing page is fully responsive with basic functionalites added, what is left is to build the product page with the fake Api

--- index.js
+++ index.js
@@ -17,11 +17,13 @@ document.addEventListener('DOMContentLoaded', () => {
   nav.appendChild(hamburger);
 
   // 4. Function to update the hamburger menu icon based on state
+  // Adding the menu list to the toggle 
   function updateIcon() {
     const img = hamburger.querySelector('img');
     const isOpen = nav.classList.contains('nav--open');
     img.src = isOpen ? './images/close-icon.svg' : './images/mobile-menu.svg';
     hamburger.setAttribute('aria-expanded', String(isOpen));
+    
   }
 
   /* 5. Apply responsive rules (mobile vs desktop) on load + resize
@@ -34,7 +36,9 @@ document.addEventListener('DOMContentLoaded', () => {
     if (isMobile) {
         hamburger.style.display = 'block';
         searchBar.style.display = 'none';
-    } else {
+        navLinks.style.left = "-100%"; // Hide menu by default on mobile
+        mobileList();  // This allows the desktop styles to not be changed
+      }else {
        hamburger.style.display = 'none';
        searchBar.style.display = 'block';
     }
@@ -46,6 +50,11 @@ document.addEventListener('DOMContentLoaded', () => {
   hamburger.addEventListener('click', () => {
     if (window.innerWidth <= 768) {
       nav.classList.toggle('nav--open');
+      if (nav.classList.contains('nav--open')) {
+        navLinks.style.left = "0";
+      } else {
+        navLinks.style.left = "-100%";
+      }
       updateIcon();
     }
   });
@@ -55,4 +64,28 @@ document.addEventListener('DOMContentLoaded', () => {
 
   // 8. Calling the responsive function
   applyResponsiveNav();
+
+  // Creating and styling the menu list that will display on toggle of the button
+function mobileList() {
+  navLinks.style.display ="flex";
+  navLinks.style.flexDirection = "column";
+  navLinks.style.position = "absolute";
+  navLinks.style.top = "75px";
+ navLinks.style.left = "-100%";
+  navLinks.style.width = "70%";
+  navLinks.style.padding = "1rem";
+  navLinks.style.backgroundColor = "#fff";
+  navLinks.style.boxShadow = "0px 4px 16px 0px #0000001a";
+  navLinks.style.zIndex = "1000";
+  navLinks.style.transition = "left 0.3s ease";
+  searchBar.style.display = "flex";
+
+  // Appending the search bar to the navlist
+  if (!navLinks.contains(searchBar)) {
+      navLinks.appendChild(searchBar);
+    }
+  
+}
+
+
 });
